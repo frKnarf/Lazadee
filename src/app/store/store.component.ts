761d5f966eeed64aae34e3a90a77ebb812163b41
frKnarf <kenthammati@gmail.com>
2023-08-07 11:24:34 +0700

    commit2

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
+import { Cart } from '../model/cart.model';
 
 @Component({
   selector: 'store',
@@ -8,12 +9,18 @@ import { ProductRepository } from '../model/product.repository';
 })
 export class StoreComponent {
   selectedCategory: string | undefined;
+  productsPerPage = 4;
+  selectedPage = 1;
 
-  constructor(private repository: ProductRepository) {}
+  constructor(private repository: ProductRepository, private cart: Cart) {}
 
   get products(): Product[] {
     //return this.repository.getProducts();
-    return this.repository.getProducts(this.selectedCategory);
+    //return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository
+      .getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
@@ -22,5 +29,29 @@ export class StoreComponent {
 
   changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(
+      Math.ceil(
+        this.repository.getProducts(this.selectedCategory).length /
+          this.productsPerPage
+      )
+    )
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
   }
 }
